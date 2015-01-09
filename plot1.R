fe526ed0ce2c57ad3e4d6f19c9a84e0f5b64c7c1
fhseymour <fhseymour1@gmail.com>
2015-01-09 13:40:28 -0700

    Adding code file plot1.R and plot file generated from code plot1.png

---
+++ plot1.R
@@ -0,0 +1,48 @@
+# Coursera John's Hopkins Exploratory Data Analysis Online Class
+# Course Project 1, code for generating Plot 1
+# Histogram of global active power for Feb 1 and 2, 2007
+# FHS Jan 9, 2015
+
+############################################################
+# Loading Data
+# On first run, loads the big all data file and creates 
+# Feb1 to Feb2 subset file for any subsequent runs.
+############################################################
+
+subsetDataFileName <- "data/subset_household_power_consumption.txt"
+if (!file.exists(subsetDataFileName)) {    
+    # no subset file, so read in all data (large file)
+    allDataFileName <- "data/household_power_consumption.txt"
+    allData <- read.table(allDataFileName, header=T, sep=";")
+
+    # Select subset data for plotting from Feb 1 and Feb 2 2007
+    allData$Date <- as.Date(allData$Date, "%d/%m/%Y") # convert text to date format
+    subsetData <- allData[allData$Date == "2007-2-1" | allData$Date == "2007-2-2",]
+    # coerce datatypes to numeric for plotting
+    subsetData$Global_active_power <- as.numeric(subsetData$Global_active_power)
+    subsetData$Global_reactive_power <- as.numeric(subsetData$Global_reactive_power)
+    subsetData$Voltage <- as.numeric(subsetData$Voltage)
+    subsetData$Sub_metering_1 <- as.numeric(subsetData$Sub_metering_1)
+    subsetData$Sub_metering_2 <- as.numeric(subsetData$Sub_metering_2)
+    subsetData$Sub_metering_3 <- as.numeric(subsetData$Sub_metering_3)
+
+    # write file of subset plotting data for future use
+    write.table(subsetData,file=subsetDataFileName,
+                quote=F, sep=";",
+                row.names=F,col.names=T)
+} else {
+    # Feb 1 and Feb 2 subset file already exists, simply read it in
+    subsetData <- read.table(subsetDataFileName, header=T, sep=";")
+}
+
+############################################################
+# Create Histogram (Plot1) and save to PNG file
+############################################################
+
+# create histogram to default display device
+hist(subsetData$Global_active_power, main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",
+     col="red")
+pngFileName <- "plots/plot1.png"
+dev.copy(png, file=pngFileName) # copy plot to a png file
+dev.off() # close png file device
